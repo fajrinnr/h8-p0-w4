1ba2c63966505ed3e9f5ce4f95d70035028cd0ee
fajrinnr <fajrin.noorrachman11@gmail.comgit config --global user.name fajrinnrgitgit config --global user.email fajrin.noorrachman11@gmail.com>
2019-11-28 18:34:46 +0700

    Logic Challenge - Angka Prima

--- exercise-1.js
+++ exercise-1.js
@@ -17,4 +17,3 @@ console.log(angkaPrima(7)); // true
 console.log(angkaPrima(6)); // false
 console.log(angkaPrima(23)); // true
 console.log(angkaPrima(33)); // false
-console.log(angkaPrima(-1)); // false
