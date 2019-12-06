5f55ecc5da4f648ccf326db40d8a09313925ab1a
fajrinnr <fajrin.noorrachman11@gmail.comgit config --global user.name fajrinnrgitgit config --global user.email fajrin.noorrachman11@gmail.com>
2019-12-07 01:37:09 +0700

    Logic Challenge: Deep Sum

--- exercise-13.js
+++ exercise-13.js
@@ -1,58 +1,58 @@
-function deepSum (arr) {
+function deepSum(arr) {
     var hasil = 0
-    if (arr.length === 0){
+    if (arr.length === 0) {
         return 'No Number'
     }
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr[i].length; j++) {
             for (let k = 0; k < arr[i][j].length; k++) {
                 hasil = hasil + arr[i][j][k]
-    
+
             }
-            
+
         }
     }
     return hasil
-  }
-  
-  //TEST CASE
-  console.log(deepSum([
+}
+
+//TEST CASE
+console.log(deepSum([
     [
-      [4, 5, 6],
-      [9, 1, 2, 10],
-      [9, 4, 3]
+        [4, 5, 6],
+        [9, 1, 2, 10],
+        [9, 4, 3]
     ],
     [
-      [4, 14, 31],
-      [9, 10, 18, 12, 20],
-      [1, 4, 90]
+        [4, 14, 31],
+        [9, 10, 18, 12, 20],
+        [1, 4, 90]
     ],
     [
-      [2, 5, 10],
-      [3, 4, 5],
-      [2, 4, 5, 10]
+        [2, 5, 10],
+        [3, 4, 5],
+        [2, 4, 5, 10]
     ]
-  ])); // 316
-  
-  console.log(deepSum([
+])); // 316
+
+console.log(deepSum([
     [
-      [20, 10],
-      [15],
-      [1, 1]
+        [20, 10],
+        [15],
+        [1, 1]
     ],
     [
-      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-      [2],
-      [9, 11]
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+        [2],
+        [9, 11]
     ],
     [
-      [3, 5, 1],
-      [1, 5, 3],
-      [1]
+        [3, 5, 1],
+        [1, 5, 3],
+        [1]
     ],
     [
-      [2]
+        [2]
     ]
-  ])); // 156
-  
-  console.log(deepSum([])); // No number
+])); // 156
+
+console.log(deepSum([])); // No number
